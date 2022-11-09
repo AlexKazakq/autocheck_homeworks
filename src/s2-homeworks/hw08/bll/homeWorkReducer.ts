17f8de43f6dd529b30a1460c1b49e0b858c04e5e
Alex <alexkazakqq@gmail.com>
2022-11-09 14:42:04 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,11 +7,9 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
             return state.sort((a,b) => action.payload === 'up'? a.name.localeCompare(b.name) : b.name.localeCompare(a.name))
         }
         case 'check': {
-
             return state.filter(el => el.age >= 18)
         }
         default:
